4e8b61748ed1c4ecbc33e3d579ff12a7f8dff0ad
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2018-11-16 19:21:15 +0300

    Fix the players/details problem on mobile

--- public/js/english/players/details.js
+++ public/js/english/players/details.js
@@ -1,34 +1,24 @@
 window.onload = () => {
-  const details = document.querySelectorAll('.details-header');
-  const videos = document.querySelectorAll('.videos-header');
+  const details = document.querySelector('.details-header');
+  const videos = document.querySelector('.videos-header');
 
-  const detailsWrapper = document.querySelectorAll('.details');
-  const videosWrapper = document.querySelectorAll('.videos');
+  const detailsWrapper = document.querySelector('.details');
+  const videosWrapper = document.querySelector('.videos');
 
-  details[0].onclick = () => {
-    detailsWrapper[0].style.display = 'block';
-    videosWrapper[0].style.display = 'none';
-    details[0].style.backgroundColor = 'rgb(212, 206, 206)';
-    videos[0].style.backgroundColor = 'white';
-  }
-  videos[0].onclick = () => {
-    detailsWrapper[0].style.display = 'none';
-    videosWrapper[0].style.display = 'block';
-    videos[0].style.backgroundColor = 'rgb(212, 206, 206)';
-    details[0].style.backgroundColor = 'white';
-  }
-
-  details[1].onclick = () => {
-    detailsWrapper[1].style.display = 'block';
-    videosWrapper[1].style.display = 'none';
-    details[1].style.backgroundColor = 'rgb(212, 206, 206)';
-    videos[1].style.backgroundColor = 'white';
-  }
-  videos[1].onclick = () => {
-    detailsWrapper[1].style.display = 'none';
-    videosWrapper[1].style.display = 'block';
-    videos[1].style.backgroundColor = 'rgb(212, 206, 206)';
-    details[1].style.backgroundColor = 'white';
-  }
- }
+  document.addEventListener('click', (event) => {
+    if (event.target) {
+      if (event.target.closest('.details-header')) {
+        detailsWrapper.style.display = 'block';
+        videosWrapper.style.display = 'none';
+        details.style.backgroundColor = 'rgb(212, 206, 206)';
+        videos.style.backgroundColor = 'white';
+      } else if (event.target.closest('.videos-header')) {
+        detailsWrapper.style.display = 'none';
+        videosWrapper.style.display = 'block';
+        videos.style.backgroundColor = 'rgb(212, 206, 206)';
+        details.style.backgroundColor = 'white';
+      }
+    }
+  });
+}
 
