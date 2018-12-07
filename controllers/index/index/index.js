528897dc268451a322d67be774ca2429ce4687b7
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2018-12-07 16:56:35 +0300

    -

--- controllers/index/index/index.js
+++ controllers/index/index/index.js
@@ -5,6 +5,7 @@ const League = require('./../../../models/league/League');
 module.exports = (req, res, next) => {
   Player
     .find()
+    .sort({"gol": -1})
     .exec(
       (err, players) => {
         if (err) return console.log(err);
