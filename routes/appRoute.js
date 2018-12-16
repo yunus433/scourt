b9acd08048ff23e41c75d4eecfb82b31f690b1fb
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2018-12-16 18:47:11 +0300

    heroku deploy issues

--- routes/appRoute.js
+++ routes/appRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer  = require('multer');
 
-const upload = multer({dest: 'public/res/uploads/'});
+const upload = multer({dest: './public/res/uploads/'});
 
 const isLoggedIn = require('./../middleware/isLoggedin');
 
