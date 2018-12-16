439037395613306ce0e3604aee9129956f917bae
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2018-12-16 19:39:14 +0300

    -

--- controllers/app/dashboard/coach/get.js
+++ controllers/app/dashboard/coach/get.js
@@ -4,39 +4,32 @@ const Team = require("../../../../models/team/Team");
 module.exports = (req, res, next) => {
   if (req.session.coach) {
     Coach.findOne({ email: req.session.coach.email }).then(coach => {
-      Team.find().exec((err, teams) => {
-        if (err) return console.log(err);
-        let cantFind = true;
+      Team.findOne({ creator: { _id: req.session.coach._id.toString() } }).exec(
+        (err, team) => {
+          if (err) return res.redirect("/");
 
-        if (teams) {
-          teams.forEach(team => {
-            if (team.creator._id == req.session.coach._id.toString()) {
-              cantFind = false;
-  
-              return res.render("app/coach/dashboard", {
-                page: "app/coach/dashboard",
-                title: "Coach Page",
-                includes: {
-                  external: ["fontawesome", "js"]
-                },
-                coach,
-                team: team
-              });
-            }
-          });
+          if (team) {
+            return res.render("app/coach/dashboard", {
+              page: "app/coach/dashboard",
+              title: "Coach Page",
+              includes: {
+                external: ["fontawesome", "js"]
+              },
+              coach,
+              team
+            });
+          } else {
+            res.render("app/coach/dashboard", {
+              page: "app/coach/dashboard",
+              title: "Coach Page",
+              includes: {
+                external: ["fontawesome", "js"]
+              },
+              coach
+            });
+          }
         }
-
-        if (cantFind) {
-          res.render("app/coach/dashboard", {
-            page: "app/coach/dashboard",
-            title: "Coach Page",
-            includes: {
-              external: ["fontawesome", "js"]
-            },
-            coach
-          });
-        }
-      });
+      );
     });
   } else {
     res.redirect("/");
